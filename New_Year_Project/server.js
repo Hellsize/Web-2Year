3cc43ef5748c83ca6f00444e0807b8ce27bbfb30
Hellsize <dmitrii-walk@mail.ru>
2021-12-12 20:00:32 +0700

    12.12

--- New_Year_Project/server.js
+++ New_Year_Project/server.js
@@ -5,14 +5,67 @@ const app = express();
 app.set("view engine",'hbs') // Поставить движок шаблонизации
 app.set("views","./templates") // размещение шаблонов
 app.use(express.static('templates'));
+games ={
+"1":{
+"game": "aloha",
+"data": "22.04.2005",
+"price":"500",
+
+},
+"2":{                    // testovo
+    "game": "hola",
+    "data": "22.12.2020",
+    "price":"1000",
+},
+
+}
+
+
+
 
 
 app.get("/",function(request,response){
-    response.sendFile(__dirname+"/index.html")
+    response.render(__dirname+"/templates/index.hbs")
        
     
     });
+    
+    
+app.get("/profile",function(request,response){
+    response.render(__dirname+"/templates/anketa.hbs")
+       
+    
+    });
+    
+app.get("/create_game",function(request,response){
+    response.render(__dirname+"/templates/game.hbs")
+       
+    
+    });
+    
+    app.get("/game/invite",function(request,response){
+        response.render(__dirname+"/templates/invite.hbs")
+           
+        
+        });
 
+        app.get("/game/anketa",function(request,response){
+            response.render(__dirname+"/templates/game_anketa.hbs")
+               
+            
+            });
+
+            app.get("/game",function(request,response){
+                
+                response.render(__dirname+"/templates/infogame.hbs")
+                
+                });
+
+                app.get("/reg",function(request,response){
+                    response.render(__dirname+"/templates/registr.hbs")
+                       
+                    
+                    });
 
 app.listen(3000,function(){
     console.log("server is on")
