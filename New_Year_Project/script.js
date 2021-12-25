9d192b83f87530a7f1ec315259a866bcb4ee5f8f
Hellsize <dmitrii-walk@mail.ru>
2021-12-26 03:18:31 +0700

    night

--- New_Year_Project/script.js
+++ New_Year_Project/script.js
@@ -1,48 +1,46 @@
-let username    = document.getElementById("username");
-let password    = document.getElementById("password");
+let username = document.getElementById("username");
+let password = document.getElementById("password");
 let loginButton = document.getElementById("login");
 
 
-loginButton.onclick = function (req,res) {
-    if ( username.value == "" || password.value == ""){
+loginButton.onclick = function(req, res) {
+    if (username.value == "" || password.value == "") {
         alert("Please fill out details!");
-    }else {
+    } else {
         document.getElementById("login").submit();
     }
 
 }
 
-function makeUrl()
-{
+function makeUrl() {
     let text = "";
     let possible = "abcdefghijklmnopqrstuvwxyz";
 
-    for( let i=0; i < 5; i++ )
+    for (let i = 0; i < 5; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
 
     return text;
 }
 
-const create_game= document.getElementById("game")
+const create_game = document.getElementById("game")
 create_game.addEventListener('click', event => {
     JSON.stringify($(form).serializeArray().reduce((acc, f) => {
         acc[f.name] = f.value
         return acc
-      }, {}))
-  });
+    }, {}))
+});
 
-  const join_game= document.getElementById("join_game")
+const join_game = document.getElementById("join_game")
 join_game.addEventListener('click', event => {
     JSON.stringify($(form).serializeArray().reduce((acc, f) => {
         acc[f.name] = f.value
         return acc
-      }, {}))
-  });
-  const user= document.getElementById("profile")
+    }, {}))
+});
+const user = document.getElementById("profile")
 user.addEventListener('click', event => {
     JSON.stringify($(form).serializeArray().reduce((acc, f) => {
         acc[f.name] = f.value
         return acc
-      }, {}))
-  });
-
+    }, {}))
+});
--- New_Year_Project/server.js
+++ New_Year_Project/server.js
@@ -2,82 +2,76 @@ const express = require("express")
 const hbs = require("hbs");
 const app = express();
 
-app.set("view engine",'hbs') // Поставить движок шаблонизации
-app.set("views","./templates") // размещение шаблонов
+app.set("view engine", 'hbs') // Поставить движок шаблонизации
+app.set("views", "./templates") // размещение шаблонов
 app.use(express.static('templates'));
 
-users={
-"1":{
-    "name":"1231313",
-    "password":"23123"
-}
+users = {
+    "1": {
+        "name": "1231313",
+        "password": "23123"
+    }
 
 }
-games ={
-"1":{
-"game": "aloha",
-"data": "22.12.2022",
-"price":"500",
+games = {
+    "1": {
+        "game": "aloha",
+        "data": "22.12.2022",
+        "price": "500",
 
-},
+    },
 
 }
 
-const urlencodedParser =express.urlencoded({extended:true});
+const urlencodedParser = express.urlencoded({ extended: true });
 
-app.post("/server",urlencodedParser,function(req,res){
-    data= req.body
+app.post("/server", urlencodedParser, function(req, res) {
+    data = req.body
     res.send(`${req.body.label_game} ${req.body.date}`);
     console.log(data)
 });
 
-app.get("/",function(request,response){
-    response.render(__dirname+"/templates/index.hbs")
-       
-    
-    });
-    
-    
-    app.get("/login",function(request,response){
-        response.render(__dirname+"/templates/registr.hbs",users)
-        let json = JSON.stringify(users);    
-        console.log(users)
-        
-        });
-    
-app.get("/profile",function(request,response){
-    response.render(__dirname+"/templates/anketa.hbs")
-       
-    
-    });
-    
-app.get("/create_game",function(request,response){
-    response.render(__dirname+"/templates/game.hbs")
-       
-    
-    });
-    
-    app.get("/game/invite",function(request,response){
-        response.render(__dirname+"/templates/invite.hbs")
-           
-        
-        });
+app.get("/", function(request, response) {
+    response.render(__dirname + "/templates/index.hbs")
 
-        app.get("/game/anketa",function(request,response){
-            response.render(__dirname+"/templates/game_anketa.hbs")
-               
-            
-            });
 
-            app.get("/game",function(request,response){
-                
-                response.render(__dirname+"/templates/infogame.hbs")
-                let name = JSON.parse(users.name)
-                });
+});
+
+
+app.get("/login", function(request, response) {
+    response.render(__dirname + "/templates/registr.hbs", users)
+    let json = JSON.stringify(users);
+    console.log(users)
+
+});
+
+app.get("/profile", function(request, response) {
+    response.render(__dirname + "/templates/anketa.hbs")
+
+
+});
+
+app.get("/create_game", function(request, response) {
+    response.render(__dirname + "/templates/game.hbs")
+
+
+});
+
+app.get("/game/invite", function(request, response) {
+    response.render(__dirname + "/templates/invite.hbs")
+
+
+});
+
+app.get("/game", function(request, response) {
+
+    response.render(__dirname + "/templates/infogame.hbs")
+    let name = JSON.parse(users.name)
+});
 
 
 
 
-app.listen(3000,function(){
+app.listen(3000, function() {
     console.log("server is on")
 });
